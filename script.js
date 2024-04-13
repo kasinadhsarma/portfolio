298a389612b75c1b7a6aff6b36bb6b137356fc74
kasinadhsarma <kasinadhsarma@gmail.com>
2024-04-13 15:38:11 +0000

    update

--- script.js
+++
@@ -1,9 +0,0 @@
-const toggleBtns = document.querySelectorAll('.toggle-btn');
-
-toggleBtns.forEach(btn => {
-  btn.addEventListener('click', () => {
-    const childContainer = btn.nextElementSibling;
-    childContainer.classList.toggle('hidden');
-    btn.textContent = childContainer.classList.contains('hidden') ? '&#9658;' : '&#9660;';
-  });
-});
