2002f667dc398aeb8b74db5ffb77bd9a1707a4af
kasinadhsarma <kasinadhsarma@gmail.com>
2024-05-11 08:01:43 +0000

    upgrade

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -1,32 +1,54 @@
 # To learn more about how to use Nix to configure your environment
 # see: https://developers.google.com/idx/guides/customize-idx-env
 { pkgs, ... }: {
-  channel = "stable-23.11"; # "stable-23.11" or "unstable"
-  # Use https://search.nixos.org/packages to  find packages
+  # Which nixpkgs channel to use.
+  channel = "stable-23.11"; # or "unstable"
+
+  # Use https://search.nixos.org/packages to find packages
   packages = [
-    pkgs.nodejs
-    pkgs.nodePackages.firebase-tools
-    pkgs.python3
+    # pkgs.go
+    # pkgs.python311
+    # pkgs.python311Packages.pip
+    # pkgs.nodejs_20
+    # pkgs.nodePackages.nodemon
   ];
+
   # Sets environment variables in the workspace
-  env = { };
-  # search for the extension on https://open-vsx.org/ and use "publisher.id"
-  idx.extensions = [
-    # "vscodevim.vim"
-  ];
-  # preview configuration
-  idx.previews = {
-    enable = true;
-    previews = [
-      {
-        command = ["python3" "-m" "http.server" "$PORT" "--bind" "0.0.0.0"];
-        manager = "web";
-        id = "web";
-      }
-      {
-        manager = "ios";
-        id = "ios";
-      }
+  env = {};
+  idx = {
+    # Search for the extensions you want on https://open-vsx.org/ and use "publisher.id"
+    extensions = [
+      # "vscodevim.vim"
     ];
+
+    # Enable previews
+    previews = {
+      enable = true;
+      previews = {
+        # web = {
+        #   # Example: run "npm run dev" with PORT set to IDX's defined port for previews,
+        #   # and show it in IDX's web preview panel
+        #   command = ["npm" "run" "dev"];
+        #   manager = "web";
+        #   env = {
+        #     # Environment variables to set for your server
+        #     PORT = "$PORT";
+        #   };
+        # };
+      };
+    };
+
+    # Workspace lifecycle hooks
+    workspace = {
+      # Runs when a workspace is first created
+      onCreate = {
+        # Example: install JS dependencies from NPM
+        # npm-install = 'npm install';
+      };
+      onStart = {
+        # Example: start a background task to watch and re-build backend code
+        # watch-backend = "npm run watch-backend";
+      };
+    };
   };
 }
