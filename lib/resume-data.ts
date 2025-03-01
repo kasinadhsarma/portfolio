9579ea24a4645f43a25f148a2c60047d1723abdf
kasinadhsarma <kasinadhsarma@gmail.com>
2025-03-01 20:27:58 +0530

    feat: update resume paths and remove obsolete PDF files

--- lib/resume-data.ts
+++ lib/resume-data.ts
@@ -7,18 +7,18 @@ export type ResumeType = {
 export const resumes: ResumeType[] = [
   {
     label: "Cyber Security Resume",
-    path: "/assets/CyberSecurity.pdf",
+    path: "public/pdf/CyberSecurity.pdf",
   },
   {
     label: "AI & ML Resume",
-    path: "/assets/AI_ML.pdf",
+    path: "public/pdf/AI_ML.pdf",
   },
   {
     label: "Full Stack Resume",
-    path: "/assets/Full_stack.pdf",
+    path: "/public/pdf/Full_stack.pdf",
   },
   {
     label: "R&D Resume",
-    path: "/assets/R&D.pdf",
+    path: "/public/pdf/R&D.pdf",
   }
 ];
